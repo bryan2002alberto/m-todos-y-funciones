9190c03003a11cce68e0c985070dd1324e0b64bb
brayan <bryan.arrriaca@gmail.com>
2025-03-27 20:25:12 -0600

    unica version

---
+++ metodos y funciones.cpp
@@ -0,0 +1,49 @@
+ #include <iostream>
+using namespace std;
+/*
+programa 1
+	//funcion que sume dos valores
+int suma(int num1, int num2, int num3){
+	int resultado = 0;
+	resultado = num1+num2+num3;
+	return resultado;
+}
+//metodo y envio de parametros de preferencia
+void suma(int &num1, int &num2){
+	int resultado =0;
+	num1+=1;
+	num2+=1;
+	resultado = num1+num2;
+	cout<<resultado<<endl;
+}	
+
+	
+main() {
+	int a=10,b=20;
+	suma(a,b);
+	cout<<"a: "<<a<<endl;
+	cout<<"b: "<<b<<endl;
+	system("pause");
+}
+
+programa 2
+	//funcion que sume dos valores
+int suma(int num1, int num2){
+	int resultado =0;
+	resultado = num1+num2;
+	return resultado;
+}
+int suma(int num1, int num2,int num3){
+	int resultado =0;
+	resultado = num1+num2+num3;
+	return resultado;
+}	
+main() {
+	
+	cout <<suma(10,20)<<endl;
+	cout <<suma(10,20,40)<<endl;
+
+
+	system("pause");
+}
+*/
